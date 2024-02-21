6340b2261551d2cfaa918c07ca4c34371ccc2d30
Diego Rook <diego.cedron@tryrook.io>
2024-02-21 06:16:55 -0500

    finish footer

--- src/app/presentation/layout/components/footer/footer.tsx
+++ src/app/presentation/layout/components/footer/footer.tsx
@@ -1,4 +1,27 @@
-
+import "./footer.css";
+import LogoIcon from "../../../../../assets/logo.png";
+import { Link } from "react-scroll";
 export default function Footer() {
-    return <><div>Footer Component</div></>;
+  return (
+    <div>
+      <div className="BackgroundColor">
+        <div className="container">
+          <div className="InnerWrapper">
+            <Link className="LogoLink" to="home" smooth={true} offset={-80}>
+              <img
+                src={LogoIcon}
+                alt="DepoEventos"
+                className="LogoIcon"
+                style={{ width: "40px", marginBottom: "7px" }}
+              />
+              <h1 className="title">DepoEventos</h1>
+            </Link>
+            <Link className="BackToTop" to="home" smooth={true} offset={-80}>
+              Back to top
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
