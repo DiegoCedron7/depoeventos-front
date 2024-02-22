7faebf4299826c4ca9066339bc1dd597d9a081a3
luxho24 <luisse24@hotmail.com>
2024-02-22 03:28:16 -0500

    Add Intro v5

--- src/app/presentation/client/home/components/intro/intro.tsx
+++ src/app/presentation/client/home/components/intro/intro.tsx
@@ -2,28 +2,52 @@ import "./intro.css";
 
 export default function Intro() {
   return (
-    <div className="w-full h-screen flex flex-row relative">
-      <div className="flex relative top-14">
-        <div className="w-[60%] py-3 px-12">
-          <h1 className="font-bold text-4xl text-center mt-10">
-            Talleres extracurriculares
-          </h1>
-          <div className="font-normal text-sm mt-4">
-            Somos una empresa especializada en el desarrollo de talleres
-            deportivos para niños y adolescentes, con el objetivo de formarlos
-            integralmente en valores y fundamentos que les permitan desenvolverse
-            de manera óptima en la disciplina que elijan.
-          </div>
-        </div>
-        <img
-          src="https://w0.peakpx.com/wallpaper/58/22/HD-wallpaper-spider-man-game-art-vertical-artwork-comic-art-portrait-display-marvel-super-heroes.jpg"
-          alt=""
-          className="h-[62%] w-[22%] object-cover rounded-xl shadow-2xl"
-        />
+
+    <div className="flex flex-row h-screen">
+      <div className="w-1/2 flex flex-col justify-center items-center bg-slate-500">
+        <h1>
+          Talleres extracurriculares
+        </h1>
+        <p className="font-normal text-sm mt-4">
+          Somos una empresa especializada en el desarrollo de talleres
+          deportivos para niños y adolescentes, con el objetivo de formarlos
+          integralmente en valores y fundamentos que les permitan desenvolverse
+          de manera óptima en la disciplina que elijan.
+        </p>
       </div>
-      <div className="w-[30%] h-[65.5%] bg-[#f5f5f5] absolute right-0 top-0 -z-50">
-        <svg className="absolute top-[48%] right-[42%] -z-20" xmlns="http://www.w3.org/2000/svg" width="186.223" height="249.21" viewBox="0 0 186.223 249.21"><g data-name="Group 102" fill="#bebdce"><g data-name="Group 59" transform="translate(-.09 .208)"><ellipse data-name="Ellipse 1" cx="4" cy="5" rx="4" ry="5" transform="translate(.228 -.08)"></ellipse><circle data-name="Ellipse 2" cx="4.565" cy="4.565" r="4.565" transform="translate(.09 39.806)"></circle><circle data-name="Ellipse 3" cx="4.565" cy="4.565" r="4.565" transform="translate(.09 79.819)"></circle><circle data-name="Ellipse 4" cx="4.565" cy="4.565" r="4.565" transform="translate(.09 119.833)"></circle><circle data-name="Ellipse 5" cx="4.565" cy="4.565" r="4.565" transform="translate(.09 159.846)"></circle><circle data-name="Ellipse 6" cx="4.565" cy="4.565" r="4.565" transform="translate(.09 199.859)"></circle><circle data-name="Ellipse 7" cx="4.565" cy="4.565" r="4.565" transform="translate(.09 239.873)"></circle></g><g data-name="Group 60" transform="translate(35.419)"><circle data-name="Ellipse 8" cx="4.565" cy="4.565" r="4.565"></circle><circle data-name="Ellipse 9" cx="4.565" cy="4.565" r="4.565" transform="translate(0 40.014)"></circle><circle data-name="Ellipse 10" cx="4.565" cy="4.565" r="4.565" transform="translate(0 80.027)"></circle><circle data-name="Ellipse 11" cx="4.565" cy="4.565" r="4.565" transform="translate(0 120.041)"></circle><circle data-name="Ellipse 12" cx="4.565" cy="4.565" r="4.565" transform="translate(0 160.054)"></circle><circle data-name="Ellipse 13" cx="4.565" cy="4.565" r="4.565" transform="translate(0 200.067)"></circle><circle data-name="Ellipse 14" cx="4.565" cy="4.565" r="4.565" transform="translate(0 240.081)"></circle></g><g data-name="Group 61" transform="translate(70.838)"><circle data-name="Ellipse 15" cx="4.565" cy="4.565" r="4.565"></circle><circle data-name="Ellipse 16" cx="4.565" cy="4.565" r="4.565" transform="translate(0 40.014)"></circle><circle data-name="Ellipse 17" cx="4.565" cy="4.565" r="4.565" transform="translate(0 80.027)"></circle><circle data-name="Ellipse 18" cx="4.565" cy="4.565" r="4.565" transform="translate(0 120.041)"></circle><circle data-name="Ellipse 19" cx="4.565" cy="4.565" r="4.565" transform="translate(0 160.054)"></circle><circle data-name="Ellipse 20" cx="4.565" cy="4.565" r="4.565" transform="translate(0 200.067)"></circle><circle data-name="Ellipse 21" cx="4.565" cy="4.565" r="4.565" transform="translate(0 240.081)"></circle></g><g data-name="Group 62" transform="translate(106.256)"><circle data-name="Ellipse 22" cx="4.565" cy="4.565" r="4.565"></circle><circle data-name="Ellipse 23" cx="4.565" cy="4.565" r="4.565" transform="translate(0 40.014)"></circle><circle data-name="Ellipse 24" cx="4.565" cy="4.565" r="4.565" transform="translate(0 80.027)"></circle><circle data-name="Ellipse 25" cx="4.565" cy="4.565" r="4.565" transform="translate(0 120.041)"></circle><circle data-name="Ellipse 26" cx="4.565" cy="4.565" r="4.565" transform="translate(0 160.054)"></circle><circle data-name="Ellipse 27" cx="4.565" cy="4.565" r="4.565" transform="translate(0 200.067)"></circle><circle data-name="Ellipse 28" cx="4.565" cy="4.565" r="4.565" transform="translate(0 240.081)"></circle></g><g data-name="Group 63" transform="translate(141.675)"><circle data-name="Ellipse 29" cx="4.565" cy="4.565" r="4.565"></circle><circle data-name="Ellipse 30" cx="4.565" cy="4.565" r="4.565" transform="translate(0 40.014)"></circle><circle data-name="Ellipse 31" cx="4.565" cy="4.565" r="4.565" transform="translate(0 80.027)"></circle><circle data-name="Ellipse 32" cx="4.565" cy="4.565" r="4.565" transform="translate(0 120.041)"></circle><circle data-name="Ellipse 33" cx="4.565" cy="4.565" r="4.565" transform="translate(0 160.054)"></circle><circle data-name="Ellipse 34" cx="4.565" cy="4.565" r="4.565" transform="translate(0 200.067)"></circle><circle data-name="Ellipse 35" cx="4.565" cy="4.565" r="4.565" transform="translate(0 240.081)"></circle></g><g data-name="Group 64" transform="translate(177.094)"><circle data-name="Ellipse 36" cx="4.565" cy="4.565" r="4.565"></circle><circle data-name="Ellipse 37" cx="4.565" cy="4.565" r="4.565" transform="translate(0 40.014)"></circle><circle data-name="Ellipse 38" cx="4.565" cy="4.565" r="4.565" transform="translate(0 80.027)"></circle><circle data-name="Ellipse 39" cx="4.565" cy="4.565" r="4.565" transform="translate(0 120.041)"></circle><circle data-name="Ellipse 40" cx="4.565" cy="4.565" r="4.565" transform="translate(0 160.054)"></circle><circle data-name="Ellipse 41" cx="4.565" cy="4.565" r="4.565" transform="translate(0 200.067)"></circle><circle data-name="Ellipse 42" cx="4.565" cy="4.565" r="4.565" transform="translate(0 240.081)"></circle></g></g></svg>
+
+      <div className="w-1/2 bg-orange-300 relative">
+        <div className="w-[35.5rem] h-[39.5rem] bg-[#f5f5f5] top-0 right-0 absolute z-5"></div>
+        <div className="bg-green-600 h-[39rem] w-[27rem] rounded-2xl top-12 left-44 absolute"></div>
       </div>
     </div>
+
+    // <div className="w-full h-screen flex flex-row relative">
+    //   {/* Contenedor Texto */}
+    //   <div className="flex relative top-12">
+    //     <div className="w-[60%] py-3 px-12">
+    //       <h1 className="font-extrabold text-5xl mt-10">
+    //         Talleres <br /> extracurriculares
+    //       </h1>
+    //       <div className="font-normal text-sm mt-4">
+    //         Somos una empresa especializada en el desarrollo de talleres
+    //         deportivos para niños y adolescentes, con el objetivo de formarlos
+    //         integralmente en valores y fundamentos que les permitan desenvolverse
+    //         de manera óptima en la disciplina que elijan.
+    //       </div>
+    //     </div>
+    //   </div>
+    //   {/* Contenedor Imagenes */}
+    //   <div>
+    //     <img
+    //       src="https://w0.peakpx.com/wallpaper/58/22/HD-wallpaper-spider-man-game-art-vertical-artwork-comic-art-portrait-display-marvel-super-heroes.jpg"
+    //       alt=""
+    //       className="h-[68%] w-[24%] object-cover rounded-xl shadow-2xl"
+    //     />
+    //     <div className="w-[27%] h-[70.5%] bg-[#f5f5f5] absolute right-0 top-0 -z-50">
+    //       <svg className="absolute top-[30%] w-40 right-[37%] -z-20" xmlns="http://www.w3.org/2000/svg" width="186.223" height="249.21" viewBox="0 0 186.223 249.21"><g data-name="Group 102" fill="#bebdce"><g data-name="Group 59" transform="translate(-.09 .208)"><ellipse data-name="Ellipse 1" cx="4" cy="5" rx="4" ry="5" transform="translate(.228 -.08)"></ellipse><circle data-name="Ellipse 2" cx="4.565" cy="4.565" r="4.565" transform="translate(.09 39.806)"></circle><circle data-name="Ellipse 3" cx="4.565" cy="4.565" r="4.565" transform="translate(.09 79.819)"></circle><circle data-name="Ellipse 4" cx="4.565" cy="4.565" r="4.565" transform="translate(.09 119.833)"></circle><circle data-name="Ellipse 5" cx="4.565" cy="4.565" r="4.565" transform="translate(.09 159.846)"></circle><circle data-name="Ellipse 6" cx="4.565" cy="4.565" r="4.565" transform="translate(.09 199.859)"></circle><circle data-name="Ellipse 7" cx="4.565" cy="4.565" r="4.565" transform="translate(.09 239.873)"></circle></g><g data-name="Group 60" transform="translate(35.419)"><circle data-name="Ellipse 8" cx="4.565" cy="4.565" r="4.565"></circle><circle data-name="Ellipse 9" cx="4.565" cy="4.565" r="4.565" transform="translate(0 40.014)"></circle><circle data-name="Ellipse 10" cx="4.565" cy="4.565" r="4.565" transform="translate(0 80.027)"></circle><circle data-name="Ellipse 11" cx="4.565" cy="4.565" r="4.565" transform="translate(0 120.041)"></circle><circle data-name="Ellipse 12" cx="4.565" cy="4.565" r="4.565" transform="translate(0 160.054)"></circle><circle data-name="Ellipse 13" cx="4.565" cy="4.565" r="4.565" transform="translate(0 200.067)"></circle><circle data-name="Ellipse 14" cx="4.565" cy="4.565" r="4.565" transform="translate(0 240.081)"></circle></g><g data-name="Group 61" transform="translate(70.838)"><circle data-name="Ellipse 15" cx="4.565" cy="4.565" r="4.565"></circle><circle data-name="Ellipse 16" cx="4.565" cy="4.565" r="4.565" transform="translate(0 40.014)"></circle><circle data-name="Ellipse 17" cx="4.565" cy="4.565" r="4.565" transform="translate(0 80.027)"></circle><circle data-name="Ellipse 18" cx="4.565" cy="4.565" r="4.565" transform="translate(0 120.041)"></circle><circle data-name="Ellipse 19" cx="4.565" cy="4.565" r="4.565" transform="translate(0 160.054)"></circle><circle data-name="Ellipse 20" cx="4.565" cy="4.565" r="4.565" transform="translate(0 200.067)"></circle><circle data-name="Ellipse 21" cx="4.565" cy="4.565" r="4.565" transform="translate(0 240.081)"></circle></g><g data-name="Group 62" transform="translate(106.256)"><circle data-name="Ellipse 22" cx="4.565" cy="4.565" r="4.565"></circle><circle data-name="Ellipse 23" cx="4.565" cy="4.565" r="4.565" transform="translate(0 40.014)"></circle><circle data-name="Ellipse 24" cx="4.565" cy="4.565" r="4.565" transform="translate(0 80.027)"></circle><circle data-name="Ellipse 25" cx="4.565" cy="4.565" r="4.565" transform="translate(0 120.041)"></circle><circle data-name="Ellipse 26" cx="4.565" cy="4.565" r="4.565" transform="translate(0 160.054)"></circle><circle data-name="Ellipse 27" cx="4.565" cy="4.565" r="4.565" transform="translate(0 200.067)"></circle><circle data-name="Ellipse 28" cx="4.565" cy="4.565" r="4.565" transform="translate(0 240.081)"></circle></g><g data-name="Group 63" transform="translate(141.675)"><circle data-name="Ellipse 29" cx="4.565" cy="4.565" r="4.565"></circle><circle data-name="Ellipse 30" cx="4.565" cy="4.565" r="4.565" transform="translate(0 40.014)"></circle><circle data-name="Ellipse 31" cx="4.565" cy="4.565" r="4.565" transform="translate(0 80.027)"></circle><circle data-name="Ellipse 32" cx="4.565" cy="4.565" r="4.565" transform="translate(0 120.041)"></circle><circle data-name="Ellipse 33" cx="4.565" cy="4.565" r="4.565" transform="translate(0 160.054)"></circle><circle data-name="Ellipse 34" cx="4.565" cy="4.565" r="4.565" transform="translate(0 200.067)"></circle><circle data-name="Ellipse 35" cx="4.565" cy="4.565" r="4.565" transform="translate(0 240.081)"></circle></g><g data-name="Group 64" transform="translate(177.094)"><circle data-name="Ellipse 36" cx="4.565" cy="4.565" r="4.565"></circle><circle data-name="Ellipse 37" cx="4.565" cy="4.565" r="4.565" transform="translate(0 40.014)"></circle><circle data-name="Ellipse 38" cx="4.565" cy="4.565" r="4.565" transform="translate(0 80.027)"></circle><circle data-name="Ellipse 39" cx="4.565" cy="4.565" r="4.565" transform="translate(0 120.041)"></circle><circle data-name="Ellipse 40" cx="4.565" cy="4.565" r="4.565" transform="translate(0 160.054)"></circle><circle data-name="Ellipse 41" cx="4.565" cy="4.565" r="4.565" transform="translate(0 200.067)"></circle><circle data-name="Ellipse 42" cx="4.565" cy="4.565" r="4.565" transform="translate(0 240.081)"></circle></g></g></svg>
+    //     </div>
+    //   </div>
+    // </div>
   );
-}
+} 
