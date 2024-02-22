302f241cb720fc7c38911fddfbe0c19babde5214
Diego Rook <diego.cedron@tryrook.io>
2024-02-21 22:17:49 -0500

    avance: footer

--- src/app/presentation/layout/components/footer/footer.tsx
+++ src/app/presentation/layout/components/footer/footer.tsx
@@ -1,26 +1,65 @@
 import "./footer.css";
 import LogoIcon from "../../../../../assets/logo.png";
 import { Link } from "react-scroll";
+import { CLIENT_ROUTES } from "../../../../shared/routes/client.routes";
+import { FaInstagramSquare } from "react-icons/fa";
+import { FaXTwitter } from "react-icons/fa6";
+import { FaFacebook } from "react-icons/fa";
+import { FaPinterest } from "react-icons/fa";
+
 export default function Footer() {
   return (
-    <div>
-      <div className="BackgroundColor">
-        <div className="container">
-          <div className="InnerWrapper">
-            <Link className="LogoLink" to="home" smooth={true} offset={-80}>
-              <img
-                src={LogoIcon}
-                alt="DepoEventos"
-                className="LogoIcon"
-                style={{ width: "40px", marginBottom: "7px" }}
-              />
-              <h1 className="title">DepoEventos</h1>
-            </Link>
-            <Link className="BackToTop" to="home" smooth={true} offset={-80}>
-              Back to top
-            </Link>
-          </div>
-        </div>
+    <div className="bg-white w-full h-[70px] flex flex-row items-center justify-center px-10 ">
+      {/* <img className="h-[40px]" src={LogoIcon} alt="logo" /> */}
+      <h1 className="text-xl italic font-bold">
+        Depo<span className="text-orange-600">Eventos</span>
+      </h1>
+      <div className="m-auto">
+        <Link className="text-slate-500 mx-4 pointer" to={CLIENT_ROUTES.HOME}>
+          Home
+        </Link>
+        <Link className="text-slate-500 mx-4 pointer" to={CLIENT_ROUTES.PACKS}>
+          Packs
+        </Link>
+        <Link className="text-slate-500 mx-4 pointer" to={CLIENT_ROUTES.EVENTS}>
+          Events
+        </Link>
+        <Link
+          className="text-slate-500 mx-4 pointer"
+          to={CLIENT_ROUTES.COURSES}
+        >
+          Courses
+        </Link>
+      </div>
+      <div className="flex flex-row gap-2 text-2xl ">
+        <a
+          className="text-slate-600"
+          href="https://www.facebook.com"
+          target="_blank"
+        >
+          <FaFacebook />
+        </a>
+        <a
+          className="text-slate-600"
+          href="https://www.instagram.com"
+          target="_blank"
+        >
+          <FaInstagramSquare />
+        </a>
+        <a
+          className="text-slate-600"
+          href="https://www.twitter.com"
+          target="_blank"
+        >
+          <FaXTwitter />
+        </a>
+        <a
+          className="text-slate-600"
+          href="https://www.pinterest.com"
+          target="_blank"
+        >
+          <FaPinterest />
+        </a>
       </div>
     </div>
   );
